4896376b17aeffee9190648bfb1daca0d538a01f
Ignacio Orlandoni <ignacio@softwareis.cool>
2020-12-21 09:54:29 -0500

    Removed configurations and added constructor

--- ASPNetCoreWebApp/Model/Model.cs
+++ ASPNetCoreWebApp/Model/Model.cs
@@ -8,8 +8,10 @@ namespace EFGetStarted
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Post> Posts { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlite("Data Source=blogging.db");
+        public BloggingContext(DbContextOptions options) : base(options)
+        {
+
+        }
     }
 
     public class Blog
