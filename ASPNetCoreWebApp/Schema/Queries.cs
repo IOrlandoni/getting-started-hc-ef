7507629f9e95c1975f16b67fa9b153045b63d190
Ignacio Orlandoni <ignacio@softwareis.cool>
2020-12-22 07:59:58 -0500

    Added root queries

---
+++ ASPNetCoreWebApp/Schema/Queries.cs
@@ -0,0 +1,27 @@
+﻿using EFGetStarted;
+using HotChocolate;
+using HotChocolate.Data;
+using HotChocolate.Types;
+using System.Linq;
+
+namespace ASPNetCoreWebApp.Schema
+{
+    public partial class Queries
+    {
+        // The order of these HC annotations matters!
+        [UseDbContext(typeof(BloggingContext))]
+        [UsePaging]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<Blog> GetBlogs([ScopedService] BloggingContext dbContext)
+            => dbContext.Blogs;
+
+        // The order of these HC annotations matters!
+        [UseDbContext(typeof(BloggingContext))]
+        [UsePaging]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<Post> GetPosts([ScopedService] BloggingContext dbContext)
+            => dbContext.Posts;
+    }
+}
--- ASPNetCoreWebApp/Startup.cs
+++ ASPNetCoreWebApp/Startup.cs
@@ -1,3 +1,4 @@
+using ASPNetCoreWebApp.Schema;
 using EFGetStarted;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -26,7 +27,10 @@ namespace ASPNetCoreWebApp
 
             // Build our schema and register it.
             services
-                .AddGraphQLServer();
+                .AddGraphQLServer()
+                .AddQueryType<Queries>()
+                .AddFiltering()
+                .AddSorting();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
